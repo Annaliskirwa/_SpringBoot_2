2acb2e93530ee9fafbada5ce38293a70cabbcb98
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-16 10:31:52 +0300

    replace number start with 0

--- src/main/java/com/example/PhoneNumber/controllers/PhoneController.java
+++ src/main/java/com/example/PhoneNumber/controllers/PhoneController.java
@@ -47,6 +47,10 @@ public class PhoneController {
         if (phoneNumber.equals("")){
             return "";
         }
+        if (phoneNumber.length() < 11 & phoneNumber.startsWith("0")){
+            String p = phoneNumber.replaceFirst("^0","+254-");
+            return p;
+        }
     }
 }
 
