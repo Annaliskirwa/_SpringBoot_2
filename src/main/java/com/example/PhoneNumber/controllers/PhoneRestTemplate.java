381b21272aa0e41188f4c4ea80dfe08aaabcadd0
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-16 12:35:12 +0300

    will use HttpHeaders to set request header

--- src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
+++ src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
@@ -1,11 +1,22 @@
 package com.example.PhoneNumber.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.HttpHeaders;
+import java.util.Arrays;
 
 @RestController
 public class PhoneRestTemplate {
     @Autowired
     RestTemplate restTemplate;
+
+    @RequestMapping(value = "template/phonenumber")
+    public String getPhoneNumbers(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    }
 }
