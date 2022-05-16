b4f0b67723dff86cfd57b43bb1a2ee4cccf473f8
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-16 12:23:41 +0300

    created a bean for rest template

--- src/main/java/com/example/PhoneNumber/PhoneNumberApplication.java
+++ src/main/java/com/example/PhoneNumber/PhoneNumberApplication.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "PhoneNumber API", version = "1.0", description = "Phone number endpoints documentation"))
@@ -11,6 +13,10 @@ public class PhoneNumberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhoneNumberApplication.class, args);
+	}
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
 
 }
