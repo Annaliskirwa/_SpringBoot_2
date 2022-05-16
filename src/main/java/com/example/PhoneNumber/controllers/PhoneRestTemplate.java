12e3f68988fbc62aeeaa9e55dbf3dbcfdaa1b94d
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-16 16:53:23 +0300

    update number controller

--- src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
+++ src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
@@ -6,10 +6,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -35,5 +32,15 @@ public class PhoneRestTemplate {
         HttpEntity<PhoneNumber> entity = new HttpEntity<PhoneNumber>(phoneNumber, headers);
 
         return restTemplate.exchange("http://localhost:8080/phonenumber/v1/phonenumber", HttpMethod.POST, entity, String.class).getBody();
+    }
+
+    @RequestMapping(value = "template/phonenumber/{id}", method = RequestMethod.PUT)
+    public String updatePhoneNumbers(@PathVariable(value = "id") String id, @RequestBody PhoneNumber phoneNumber){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<PhoneNumber> entity = new HttpEntity<PhoneNumber>(phoneNumber, headers);
+
+        return restTemplate.exchange(" http://localhost:8080/phonenumber/v1/phonenumber/"+id, HttpMethod.PUT, entity, String.class).getBody();
+
     }
 }
