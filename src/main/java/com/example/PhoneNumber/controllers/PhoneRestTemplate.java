de374cdf6a9798b015fbb5e61aeac30bbd40c8e6
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-16 13:04:10 +0300

    consuming POST api using rest template

--- src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
+++ src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
@@ -1,14 +1,17 @@
 package com.example.PhoneNumber.controllers;
 
+import com.example.PhoneNumber.model.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import org.springframework.http.HttpHeaders;
 import java.util.Arrays;
 
 @RestController
@@ -23,5 +26,14 @@ public class PhoneRestTemplate {
         HttpEntity <String> entity =  new HttpEntity<String>(headers);
 
         return restTemplate.exchange("http://localhost:8080/phonenumber/v1/phonenumber", HttpMethod.GET, entity, String.class).getBody();
+    }
+
+    @RequestMapping(value = "template/phonenumber", method = RequestMethod.POST)
+    public String createPhoneNumbers(@RequestBody PhoneNumber phoneNumber){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<PhoneNumber> entity = new HttpEntity<PhoneNumber>(phoneNumber, headers);
+
+        return restTemplate.exchange("http://localhost:8080/phonenumber/v1/phonenumber", HttpMethod.POST, entity, String.class).getBody();
     }
 }
