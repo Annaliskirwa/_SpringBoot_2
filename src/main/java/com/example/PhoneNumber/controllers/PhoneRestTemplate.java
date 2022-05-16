b3fafbee622abc5c4f82a8dbfc241edf26832f9d
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-16 12:36:38 +0300

    will use HttpEntity to wrap the request object

--- src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
+++ src/main/java/com/example/PhoneNumber/controllers/PhoneRestTemplate.java
@@ -1,6 +1,7 @@
 package com.example.PhoneNumber.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,6 @@ public class PhoneRestTemplate {
     public String getPhoneNumbers(){
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <String> entity =  new HttpEntity<String>(headers);
     }
 }
